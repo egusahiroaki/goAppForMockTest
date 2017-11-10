47b6b200c53609ceac743dcbaa9c6d2213359c5c
egusahiroaki <egusa.hiroaki@gmail.com>
2017-11-10 19:27:36 +0900

    delete comment out

--- mysqlSample/main.go
+++ mysqlSample/main.go
@@ -4,9 +4,6 @@ import "github.com/goSamples/mysqlSample/service"
 import "fmt"
 
 func main() {
-	// r := model.NewUserPointRepository()
-	// result := r.GetTotalPointByUserID(1)
-	// fmt.Println(result)
 	s := service.NewUserService()
 	point := s.GetTotalPointByID(1)
 	fmt.Printf("point: %v\n", point)
